7e98d988b164641c5c0faac1b340c0399271ee49
Timo Gruen <timo.gruen@raytion.com>
2018-10-12 22:11:40 +0200

    first commit

---
+++ src/main/kotlin/com/timogruen/kubernetes/example/timeservice/TimeService.kt
@@ -0,0 +1,30 @@
+package com.timogruen.kubernetes.example.timeservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.client.RestTemplate
+import java.net.InetAddress
+import java.time.LocalDateTime
+
+@SpringBootApplication
+@RequestMapping(value = ["/api/v1"])
+class TimeService {
+    @RequestMapping(value = ["/message/{string}"], method = [RequestMethod.GET])
+    fun getTime(string: String): ResponseEntity<String> {
+        val template = RestTemplate()
+        val time = template.getForEntity(
+                "http://timeservice/api/v1/time",
+                String::class.java)
+        return ResponseEntity.ok("$time: $string ")
+    }
+
+    @RequestMapping(value = ["/shutdown"], method = [RequestMethod.POST])
+    fun shutdown() = System.exit(0)
+}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(TimeService::class.java, *args)
+}
