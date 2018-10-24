e4825f5c9bd1a9600b295b8e7aa6ff26889967db
Timo Grün <Timo.Gruen@uni-duesseldorf.de>
2018-10-24 22:15:42 +0200

    refactor service

--- Makefile
+++ Makefile
@@ -1,6 +1,19 @@
 compile:
 	gradle build
+
 dockerImage: compile
-	docker build -t timogruen.com/kubernetes/example/messageservice .
+	docker build -t timogruen.com/kubernetes/example/messageservice:1.0.0 .
+
 dockerCompose: dockerImage
 	cd ./docker && docker-compose up -d
+
+kubectlDeleteServices:
+	kubectl delete services messageservice \
+  && kubectl delete services timeservice
+
+kubectlDeleteDeployment: kubectlDeleteServices
+	kubectl delete deployments messageservice \
+	&& kubectl delete deployments timeservice
+
+komposeUp:
+	cd ./docker && kompose up
--- src/main/kotlin/com/timogruen/kubernetes/example/messageservice/MessageService.kt
+++ src/main/kotlin/com/timogruen/kubernetes/example/messageservice/MessageService.kt
@@ -1,8 +1,10 @@
 package com.timogruen.kubernetes.example.messageservice
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.client.RestTemplate
@@ -11,14 +13,17 @@ import java.net.InetAddress
 
 @SpringBootApplication
 @RequestMapping(value = ["/api/v1"])
-class TimeService {
+class MessageService {
 
-    @RequestMapping(value = ["/message"], method = [RequestMethod.GET])
-    fun getMessage(): ResponseEntity<String> {
+    val log = LoggerFactory.getLogger(MessageService::class.java)
+
+    @RequestMapping(value = ["/message/{text}"], method = [RequestMethod.GET])
+    fun getMessage(@PathVariable("text") text: String): ResponseEntity<String> {
+        log.info("Message received with Text='$text'")
         val template = RestTemplate()
         val time = template.getForEntity("http://timeservice:9000/api/v1/time", String::class.java)
         val ip = InetAddress.getByName("timeservice")
-        return ResponseEntity.ok("[${ip.hostAddress}] - ${time.body}: Hello World!")
+        return ResponseEntity.ok("[${ip.hostAddress}] - ${time.body}: Hello $text! \n")
     }
 
     @RequestMapping(value = ["/shutdown"], method = [RequestMethod.POST])
@@ -26,5 +31,5 @@ class TimeService {
 }
 
 fun main(args: Array<String>) {
-    SpringApplication.run(TimeService::class.java, *args)
+    SpringApplication.run(MessageService::class.java, *args)
 }
